61b2afd1ac6d48b2ba77c42fb04bafe3e4d2d837
GiK <gladyi@hotamil.com>
2022-02-18 23:04:59 +0300

    client part websocket rabbitmq

--- spring-mvc/src/main/java/com/example/springmvc/PositionRetriever.java
+++ spring-mvc/src/main/java/com/example/springmvc/PositionRetriever.java
@@ -3,14 +3,17 @@ package com.example.springmvc;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.function.Consumer;
 
 @Configuration
 @AllArgsConstructor
 public class PositionRetriever {
-
+    private WebSocketHandler handler;
     private AircraftRepository repo;
 
     @Bean
@@ -20,7 +23,19 @@ public class PositionRetriever {
 
             repo.saveAll(acList);
 
-            repo.findAll().forEach(System.out::println);
+            sendPositions();
         };
+    }
+
+    private void sendPositions() {
+        if (repo.count() > 0) {
+            for (WebSocketSession sessionInList : handler.getSessionList()) {
+                try {
+                    sessionInList.sendMessage(new TextMessage(repo.findAll().toString()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
